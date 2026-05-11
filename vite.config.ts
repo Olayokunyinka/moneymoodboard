aa3342a6c05caca41de9b0857fdc4f383c533804
Olayinka Olayokun <13032616+Olayokunyinka@users.noreply.github.com>
2026-05-11 15:22:33 +0100

    Update TanStack versions, add engines, switch back to SSR with correct config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 // @ts-check
 import { defineConfig } from "vite";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import { tanStackStartVite } from "@tanstack/start-plugin-core/vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [
     TanStackRouterVite(),
+    tanStackStartVite(),
     tsConfigPaths(),
     tailwindcss(),
   ],
